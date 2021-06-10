189f107f7207be65971016a7002ccec3995b0117
Abeshi2002 <s1290128@u-aizu.ac.jp>
2021-06-10 17:35:14 +0900

    prime factorization

--- git_learn/factorize.cpp
+++ git_learn/factorize.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 int main(){
   long n, i; cin >> n;
-  bool not1st = true;
+  bool not1st = false;
   
-  if(n == 1 && n == 0) cout << n <<" = " << n << endl;
-  if(n < 0) cout << "Don't type a negative integer!" << endl;
+  if(n == 1 || n == 0) cout << n <<" = " << n << endl;
+  else if(n < 0) cout << "Don't type a negative integer!" << endl;
   else{
     cout << n << " = ";
     for(i=2; i*i < n; i++){
       while(n%i == 0){
         n /= i;
-        if(not1st) cout << " * ";
+        if(not1st){
+            cout << " * ";
+	    not1st = true;
+	}
 	cout << i;
       }
     }
